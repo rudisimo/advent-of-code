15565f152ecf04652d904870e7d0ee150906e5a8
Rodolfo Puig <rodolfo@puig.io>
2022-12-06 19:21:42 +0000

    Updating pytest fixture loading mechanism

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,29 +2,52 @@ import importlib
 
 from glob import glob
 from pathlib import Path
-from typing import List, Optional, Tuple
+from typing import List
 
 import pytest
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "-Y",
+        action="store",
+        metavar="YYYY",
+        help="only run tests matching the year YYYY.",
+    )
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "year(YYYY): mark test to run only for specific year")
+
+
+def pytest_runtest_setup(item):
+    years = [mark.args[0] for mark in item.iter_markers(name="year")]
+    if years:
+        year_opt = item.config.getoption("-Y")
+        if year_opt and year_opt not in years:
+            pytest.skip(f"test requires year to be in [{', '.join(years)}]")
+
+
 @pytest.fixture
 def solve_puzzle():
-    def wrapper(year: int, day: int, part: int, data: List[str]) -> Optional[str]:
+    def wrapper(year: int, day: int, part: int, fixture_data: List[str]):
         try:
-            solver_fn = getattr(importlib.import_module(f"aoc.{year}.day_{day:0{2}}"), f"answer_{part}")
-        except (AttributeError, ModuleNotFoundError) as e:
-            raise NotImplementedError(e)
+            solver_module = f"aoc.{year}.day_{day:0{2}}"
+            solver_attr = f"answer_{part}"
+            solver_fn = getattr(importlib.import_module(solver_module), solver_attr)
+        except (AttributeError, ModuleNotFoundError):
+            pytest.skip(f"solver not found: {solver_module}:{solver_attr}")
         else:
-            return str(solver_fn(data))
+            return str(solver_fn(fixture_data))
 
     return wrapper
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def load_fixtures():
-    def wrapper(year: int, day: int, part: int, fixtures: List[str]) -> Tuple[List[str], Optional[str]]:
-        for fixture_name in fixtures:
-            fixture_glob = f"{Path().absolute()}/tests/fixtures/{year}/{day:0{2}}-{fixture_name}.txt"
+    def wrapper(year: int, day: int, part: int, fixture_names: List[str]):
+        for fixture_name in fixture_names:
+            fixture_glob = f"{Path().absolute()}/tests/fixtures/{year}/{day:02}-{fixture_name}.txt"
             fixture_files = glob(fixture_glob)
             for fixture in fixture_files:
                 try:
@@ -34,6 +57,6 @@ def load_fixtures():
                 except (IndexError):
                     yield ([], None)
             if not fixture_files:
-                raise FileNotFoundError(f"fixture(s) not found: {fixture_glob}")
+                pytest.skip(f"fixture not found: {fixture_glob}")
 
     return wrapper
