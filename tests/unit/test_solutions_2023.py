e2572a7713b1ccf1b0f29d1485e7cc54e4c796d5
Rodolfo Puig <rodolfo@puig.io>
2023-12-05 15:09:42 -0500

    Updating fixture loading mechanism for 2023
    AoC changed the way the examples and the puzzle inputs/answers are
    organized.

--- tests/unit/test_solutions_2023.py
+++ tests/unit/test_solutions_2023.py
@@ -1,5 +1,5 @@
 from itertools import product
-from typing import Iterator, List
+from typing import Iterator
 
 import pytest
 
@@ -7,17 +7,21 @@ from _pytest.mark import ParameterSet
 
 
 FIXTURE_YEAR = "2023"
-FIXTURE_DATES = product(range(1, 26), range(1, 3))
-FIXTURE_NAMES = ["example", "input"]
+FIXTURE_COMPONENTS = product(range(1, 26), [("example-1", 1, 1), ("example-2", 1, 2), ("input", 1, 1), ("input", 2, 2)])
 
 
-def parametrize_fixtures(year, dates, fixtures) -> Iterator[ParameterSet]:
-    return [pytest.param(year, day, part, fixtures, id=f"{year}-{day:02}-{part}") for day, part in dates]
+def parametrize_fixtures(year, components) -> Iterator[ParameterSet]:
+    return [
+        pytest.param(year, day, part, solver, fixture, id=f"{year}-{day:02}-{solver}-{fixture}")
+        for day, (fixture, part, solver) in components
+    ]
 
 
 @pytest.mark.year(FIXTURE_YEAR)
-@pytest.mark.parametrize("year,day,part,fixtures", parametrize_fixtures(FIXTURE_YEAR, FIXTURE_DATES, FIXTURE_NAMES))
-def test_solve_puzzle_answers(year: str, day: int, part: int, fixtures: List[str], load_fixtures, solve_puzzle) -> None:
-    for (fixture_data, expected_output) in load_fixtures(year, day, part, fixtures):
-        output = solve_puzzle(year, day, part, fixture_data)
+@pytest.mark.parametrize("year,day,part,solver,fixture", parametrize_fixtures(FIXTURE_YEAR, FIXTURE_COMPONENTS))
+def test_solve_puzzle_answers(
+    year: str, day: int, part: int, solver: int, fixture: str, load_fixtures, solve_puzzle
+) -> None:
+    for (fixture_data, expected_output) in load_fixtures(year, day, part, [fixture]):
+        output = solve_puzzle(year, day, solver, fixture_data)
         assert output == expected_output
