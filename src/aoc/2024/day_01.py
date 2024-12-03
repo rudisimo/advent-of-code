ddd00aefb3637cddf0a145865b24e966c996420c
Rudy Puig <rudy.puig@boats.com>
2024-12-03 12:33:41 -0500

    chore: implement day 1 solutions

---
+++ src/aoc/2024/day_01.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from functools import reduce
+from operator import countOf
+from operator import mul
+from operator import sub
+from typing import Iterable, Iterator, List
+
+
+def answer_1(input: List[str]) -> int:
+    locations = extract_locations(input)
+    distance = calculate_distance(locations)
+    return distance
+
+
+def answer_2(input: List[str]) -> int:
+    locations = extract_locations(input)
+    distance = calculate_similarity(locations)
+    return distance
+
+
+def extract_locations(input: List[str]) -> Iterator[Iterable[int]]:
+    locations = [map(int, line.split()) for line in input]
+    yield from locations
+
+
+def calculate_distance(input: Iterator[Iterable[int]]) -> int:
+    locations = zip(*map(sorted, zip(*input)))
+    distances = [reduce(sub, sorted(v, reverse=True)) for v in locations]
+    return sum(distances)
+
+
+def calculate_similarity(input: Iterator[Iterable[int]]) -> int:
+    left, right = zip(*input)
+    similarity = [reduce(mul, (v,), countOf(right, v)) for v in left]
+    return sum(similarity)
